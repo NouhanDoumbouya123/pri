f99d479ef193440558ae4be42019b1ac05a1d383
NouhanDoumbouya123 <norassobadoumbouya123@gmail.com>
2024-01-30 22:14:15 +0800

    This is printf function to handle char, string, and % as the first task

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository is made for alx printf project, where we have to implement the printf function from scratch.
---
+++ _printf.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ * print_char - help function print
+ *  @c: the parameter
+ *  Return: number of char printed
+ */
+
+int print_char(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - help fuction print
+ *  @c: the string to be printed
+ *  Return: the number of string printed
+ */
+
+int print_string(char *c)
+{
+	int len = 0;
+
+	while (c[len] != '\0')
+		len++;
+
+	return (write(1, c, len));
+}
+/**
+ * _printf - a function print to handle c, s and %
+ * @format: the first argument in variadic
+ * Return: the number of char printed
+ */
+int _printf(const char *format, ...)
+{
+	int char_printed = 0;
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+	va_start(arg_list, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			char_printed++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				char_printed++;
+			}
+			else if (*format == 'c')
+			{
+				char_printed += print_char(va_arg(arg_list, int));
+			}
+			else if (*format == 's')
+			{
+				char_printed += print_string(va_arg(arg_list, char *));
+			}
+
+		}
+		format++;
+	}
+	va_end(arg_list);
+
+	return (char_printed);
+}
